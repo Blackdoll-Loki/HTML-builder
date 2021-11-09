3fc08452392c9de0ed2dbd18b3226f6f017923fd
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-11-09 04:02:01 +0200

    feat: solved second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const readLine = require('readline');
+const process = require('process');
+const {stdin, stdout, exit} = process;
+
+
+let writingPath = path.join(__dirname, 'input.txt');
+const writeStream = fs.createWriteStream(writingPath);
+console.log('write something');
+
+let rl = readLine.createInterface({input: stdin});
+process.on('SIGINT', function(){
+    console.log('\nGoodbuy');
+    exit();
+});
+
+rl.on('line', (text) => {
+    writeStream.write(text);
+    if(text === 'exit'){
+        console.log('Goodbuy');
+        exit();
+    }
+});
+
+
+
+//console.log('Write anything');
+//readStream.pipe(writeStream);
+
