ca9193d1f11d045c3bd43b10fb7f3a7d865319bc
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-11-09 18:52:48 +0200

    feat: solved 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+let filePath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(filePath,{ withFileTypes: false}, (err, files) => {
+    if(err){
+        console.log(err);
+    } else{
+        console.log('\nCurrent directory file names');
+        files.forEach((file) => {
+            let name = file.replace(/.\w+$/, '');
+            let ext = path.extname(file).replace('.', '');
+            fs.stat(path.join(filePath, file), (err, stats) => {
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log(`${name} - ${ext} - ${stats.size/1000}kb`);
+                }
+            });
+        });
+    }
+});
